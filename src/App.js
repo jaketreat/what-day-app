3b3c291a6a2e75362a5c284eafab4c9eaffec912
Jake Treat <jake@Jakes-MacBook-Air.local>
2026-03-31 09:02:07 -0500

    tried out dark theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { useContext } from "react";
 import Grid from "./components/Grid";
 import { AppContext } from "./context/AppContext";
+import './App.css';
+import { useState } from "react";
 
 export default function App() {
   const { dispatch } = useContext(AppContext);
+  const [theme, setTheme] = useState(false);
+
+  function toggleTheme() {
+    setTheme(!theme);
+  }
+
+  const themeClass = {
+    classToggle: theme ? "dark-theme" : "light-theme",
+  };
 
   function changeGrid() {
     const rows = Number(prompt("Rows?"));
@@ -13,11 +24,12 @@ export default function App() {
   }
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Modular Scheduler</h1>
+    <div className={themeClass.classToggle} style={{ padding: "20px" }}>
+      <h1 className={themeClass.classToggle}>Modular Scheduler</h1>
 
       {/* Change grid layout */}
       <button onClick={changeGrid}>Change Grid</button>
+      <button onClick={toggleTheme}>Change Theme</button>
 
       <Grid />
     </div>
