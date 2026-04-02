1d4b9a0e954e4383683e2f530ec75bb40e6ec63b
Jake Treat <jake@Jakes-MacBook-Air.local>
2026-04-02 11:58:34 -0500

    added styling elements

--- src/App.js
+++ src/App.js
@@ -20,12 +20,22 @@ useEffect(() => {
 
 const isGradient = activeTheme.header.includes("gradient");
 
+const buttonStyle = {
+  background: activeTheme.contrastColor, 
+  color: activeTheme.background, 
+  textDecoration: 'none', 
+  border: 'none', 
+  borderRadius: '8px',
+};
+
 const headerStyle = isGradient
   ? {
+      textAlign: 'center',
       background: activeTheme.header,
       WebkitBackgroundClip: "text",
       WebkitTextFillColor: "transparent",
-      width: "40%"
+      width: "40%",
+      margin: "auto",
     }
   : {
       color: activeTheme.header,
@@ -44,14 +54,16 @@ const headerStyle = isGradient
         <div style={{ background: activeTheme.color4, height: "50px", width: "50px", borderRadius: "10px" }}></div>
       </div>
       <div className="buttons">
-        <button onClick={() => setScheme("flower")}>Flower</button>
-        <button onClick={() => setScheme("muted")}>Muted</button>
-        <button onClick={() => setScheme("groovy")}>Groovy</button>
-        <button onClick={() => setScheme("playground")}>Playground</button>
-        <button onClick={() => setMode(mode === "light" ? "dark" : "light")}>
+        <button style={buttonStyle} onClick={() => setScheme("flower")}>Flower</button>
+        <button style={buttonStyle} onClick={() => setScheme("muted")}>Muted</button>
+        <button style={buttonStyle} onClick={() => setScheme("groovy")}>Groovy</button>
+        <button style={buttonStyle} onClick={() => setScheme("playground")}>Playground</button>
+        </div>
+        <div className="buttons">
+        <button style={buttonStyle} onClick={() => setMode(mode === "light" ? "dark" : "light")}>
           Toggle Mode
         </button>
-        <button onClick={() => setStyle(style === "solid" ? "gradient" : "solid")}>
+        <button style={buttonStyle} onClick={() => setStyle(style === "solid" ? "gradient" : "solid")}>
           Toggle Style
         </button>
       </div>
